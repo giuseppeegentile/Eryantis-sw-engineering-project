84a4616cf31f7228626f0ae77afe06ad65a70549
Davide <davidegalbiati29@gmail.com>
2022-05-01 14:51:12 +0200

    DecideOrderPlayerStateTest also works

--- src/test/java/it/polimi/ingsw/controller/game/DecideOrderPlayerStateTest.java
+++ src/test/java/it/polimi/ingsw/controller/game/DecideOrderPlayerStateTest.java
@@ -14,9 +14,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class DecideOrderPlayerStateTest {
 
     @Test
-    void firstOfFourPlayer(){
+    void orderWithFourPlayer(){
         GameModel testGame = GameModel.getInstance();
         List<PlayerModel> playersModels = new ArrayList<>();
+        List<AssistantCardModel> cemetery = new ArrayList<>();
         PlayerModel player1 = new PlayerModel("jack", ColorTower.GREY);
         playersModels.add(player1);
         PlayerModel player2 = new PlayerModel("john", ColorTower.BLACK);
@@ -26,17 +27,23 @@ class DecideOrderPlayerStateTest {
         PlayerModel player4 = new PlayerModel("quarto", ColorTower.BLACK);
         playersModels.add(player4);
         testGame.setPlayers(playersModels);
-        List<AssistantCardModel> cemetery = new ArrayList<AssistantCardModel>();
         AssistantCardModel card1 = new AssistantCardModel(4, (byte) 5);
         card1.setOwner(player1);
+        cemetery.add(card1);
         AssistantCardModel card2 = new AssistantCardModel(2, (byte) 2);
         card2.setOwner(player2);
+        cemetery.add(card2);
         AssistantCardModel card3 = new AssistantCardModel(6, (byte) 5);
         card3.setOwner(player3);
+        cemetery.add(card3);
         AssistantCardModel card4 = new AssistantCardModel(1, (byte) 3);
         card4.setOwner(player4);
+        cemetery.add(card4);
         DecideOrderPlayerState decide = new DecideOrderPlayerState(testGame);
         decide.setPlayersOrderForActionPhase(cemetery);
         assertEquals("quarto", testGame.getPhaseOrder().get(0).getNickname());
+        assertEquals("john", testGame.getPhaseOrder().get(1).getNickname());
+        assertEquals("jack", testGame.getPhaseOrder().get(2).getNickname());
+        assertEquals("al", testGame.getPhaseOrder().get(3).getNickname());
     }
 }
