ad3e8528b9c0641993159c32b1ffdecfd8ab5d0b
Christian Giosia <christian.giosia@mail.polimi.it>
2022-05-31 15:27:52 +0200

    PlayersNumberScene.fxml added

--- src/main/java/it/polimi/ingsw/view/gui/scene/PlayersNumberSceneController.java
+++ src/main/java/it/polimi/ingsw/view/gui/scene/PlayersNumberSceneController.java
@@ -1,14 +1,18 @@
 package it.polimi.ingsw.view.gui.scene;
 
 import it.polimi.ingsw.observer.ViewObservable;
+import it.polimi.ingsw.observer.ViewObserver;
+import it.polimi.ingsw.view.gui.SceneController;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
 
-public class PlayersNumberSceneController extends ViewObservable implements GenericSceneController{
+/**
+ * This class implements the scene where the game host chooses the number of players who are going to play.
+ */
+public class PlayersNumberSceneController extends ViewObservable implements GenericSceneController {
 
-    private int minPlayers=0;
-    private int maxPlayers=0;
-
-    public void setPlayersRange(int minPlayers, int maxPlayers){
-        this.minPlayers = minPlayers;
-        this.maxPlayers = maxPlayers;
-    }
 }
