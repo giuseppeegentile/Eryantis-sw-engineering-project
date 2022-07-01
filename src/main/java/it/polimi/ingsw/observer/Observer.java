3a87be0a3b73b5606dc20cf37a49f96ae2566e06
Davide <davidegalbiati29@gmail.com>
2022-07-01 20:07:27 +0200

    Fixed observable JavaDocs

--- src/main/java/it/polimi/ingsw/observer/Observer.java
+++ src/main/java/it/polimi/ingsw/observer/Observer.java
@@ -2,7 +2,9 @@ package it.polimi.ingsw.observer;
 
 import it.polimi.ingsw.network.message.Message;
 
-
+/**
+ * Observer interface. It supports a generic method of update.
+ */
 public interface Observer {
     void update(Message message);
 }
